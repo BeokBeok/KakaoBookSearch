df80da1396697edae00d27fecfb471cf927cd779
강현석 <kekemusa37@gmail.com>
2021-11-12 11:08:44 +0900

    :sparkles: [구현] Dependency 파일

---
+++ buildSrc/src/main/java/Dependency.kt
@@ -0,0 +1,9 @@
+object AndroidX {
+    const val CORE_KTX = "androidx.core:core-ktx:1.7.0"
+    const val APPCOMPAT = "androidx.appcompat:appcompat:1.3.1"
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.1"
+}
+
+object Google {
+    const val MATERIAL = "com.google.android.material:material:1.4.0"
+}
