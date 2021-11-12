0c985d16b97ee900ce303b9c02dcd5431aa881c3
강현석 <kekemusa37@gmail.com>
2021-11-12 19:11:03 +0900

    :sparkles: [구현] RecyclerView 관련 Base

---
+++ common/src/main/java/com/beok/common/base/BaseViewHolder.kt
@@ -0,0 +1,32 @@
+package com.beok.common.base
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.RecyclerView
+
+class BaseViewHolder(
+    parent: ViewGroup,
+    @LayoutRes private val layoutResourceID: Int,
+    private val bindingID: Int,
+    private val viewModel: Map<Int, ViewModel>
+) : RecyclerView.ViewHolder(
+    LayoutInflater
+        .from(parent.context)
+        .inflate(layoutResourceID, parent, false)
+) {
+    private val binding: ViewDataBinding = DataBindingUtil.bind(itemView)!!
+
+    fun bind(item: Any?) {
+        if (item == null) return
+        binding.setVariable(bindingID, item)
+
+        for (key in viewModel.keys) {
+            binding.setVariable(key, viewModel[key])
+        }
+        binding.executePendingBindings()
+    }
+}
