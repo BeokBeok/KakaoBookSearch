428568c2fa9e553258238d7fa68363e61278d572
강현석 <kekemusa37@gmail.com>
2021-11-12 11:47:18 +0900

    :sparkles: [적용] Navigation Component 디펜던시

--- buildSrc/src/main/java/Dependency.kt
+++ buildSrc/src/main/java/Dependency.kt
@@ -7,3 +7,10 @@ object AndroidX {
 object Google {
     const val MATERIAL = "com.google.android.material:material:1.4.0"
 }
+
+object Navigation {
+    private const val VERSION = "2.3.5"
+
+    const val FRAGMENT_KTX = "androidx.navigation:navigation-fragment-ktx:$VERSION"
+    const val UI_KTX = "androidx.navigation:navigation-ui-ktx:$VERSION"
+}
