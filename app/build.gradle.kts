1ab49a56a2ea09b7261db08960c75cfe61a89d4c
강현석 <kekemusa37@gmail.com>
2021-11-12 11:22:19 +0900

    :sparkles: [적용] groovy to kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    applyDefault()
+
+    defaultConfig {
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    AndroidX.run {
+        implementation(CORE_KTX)
+        implementation(APPCOMPAT)
+        implementation(CONSTRAINT_LAYOUT)
+    }
+    implementation(Google.MATERIAL)
+}
