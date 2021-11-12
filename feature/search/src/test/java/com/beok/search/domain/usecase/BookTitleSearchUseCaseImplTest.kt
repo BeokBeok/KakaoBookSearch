571a5eba1b71793f4190c1131839dc264d5d0479
강현석 <kekemusa37@gmail.com>
2021-11-12 15:50:43 +0900

    :sparkles: [구현] BookTitleSearchUseCase

---
+++ feature/search/src/test/java/com/beok/search/domain/usecase/BookTitleSearchUseCaseImplTest.kt
@@ -0,0 +1,24 @@
+package com.beok.search.domain.usecase
+
+import com.beok.search.domain.repository.BookSearchRepository
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+
+internal class BookTitleSearchUseCaseImplTest {
+
+    private val bookSearchRepository: BookSearchRepository = BookSearchRepository.Fake()
+    private val useCase: BookTitleSearchUseCase<BookTitleSearchUseCaseImpl.Param> =
+        BookTitleSearchUseCase.Fake()
+
+    @Test
+    fun `책 제목으로 검색합니다`() = runBlocking {
+        val bookTitle = "미움 받을 용기"
+        val expected = bookSearchRepository.searchBookByTitle(title = bookTitle)
+        val actual = useCase.execute(
+            param = BookTitleSearchUseCaseImpl.Param(query = bookTitle)
+        )
+
+        assertEquals(expected, actual.getOrNull())
+    }
+}
